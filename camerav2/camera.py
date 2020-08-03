14ec7294f98b9493507619451f05db1d6ba0dbbd
Yuki Horikawa <mandorinyuki@icloud.com>
2020-08-03 11:58:18 +0900

    camerav2ファイルが最新です。

---
+++ camerav2/camera.py
@@ -0,0 +1,22 @@
+import cv2
+
+# VideoCameraを追加
+class VideoCamera():
+    def __init__(self):
+        self.video = cv2.VideoCapture(2)
+   
+    def __del__(self):
+        self.video.release()
+
+    def get_frame(self):      
+        success, image = self.video.read()
+        height = image.shape[0]
+        width = image.shape[1]
+        image = cv2.resize(image , (int(width*0.5), int(height*0.5)))
+        ret, jpeg = cv2.imencode('.jpg',image)
+        return jpeg.tobytes()
+
+        
+
+    
+        
